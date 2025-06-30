9391aaed70847c8f74a73546e456be7aa1f711af
HoYeeco <hoyeeco@outlook.com>
2025-06-30 18:03:50 +0800

    主类

--- backend/src/main/java/com/cupk/backend/BackendApplication.java
+++ backend/src/main/java/com/cupk/backend/BackendApplication.java
@@ -1,12 +1,15 @@
 package com.cupk.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cupk.backend.mapper")
 public class BackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendApplication.class, args);
     }
 
