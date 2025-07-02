e42fe71f8344f98149afe20082ae092d9617c1a7
HoYeeco <hoyeeco@outlook.com>
2025-07-02 20:39:34 +0800

    update

--- backend/src/main/java/com/cupk/backend/controller/UploadController.java
+++ backend/src/main/java/com/cupk/backend/controller/UploadController.java
@@ -1,13 +1,23 @@
 package com.cupk.backend.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.cupk.backend.common.Result;
 import com.cupk.backend.mapper.UploadMapper;
 import com.cupk.backend.pojo.Upload;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
 @RestController
 @RequestMapping("/upload")
